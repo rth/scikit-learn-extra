62ecf39d04b6aa4406868402840b0f6af2b20f1e
Valentin Haenel <valentin@haenel.co>
2014-09-14 14:30:34 +0200

    split cython fht
    ...into something that can be called and something that can be reused
    fuzz testing fht2
    fix bug in fht
    simplify fht2 with reuse of _fht
    tweak the FHT implementation
    some more tweaks

--- sklearn/utils/cyfht.pyx
+++ sklearn/utils/cyfht.pyx
@@ -1,3 +1,4 @@
+import numpy as np
 cimport numpy as np
 cimport cython
 from libc.math cimport log2
@@ -11,13 +12,20 @@ def is_power_of_two(input_integer):
 #DTYPE = np.double
 ctypedef np.double_t DTYPE_t
 
-@cython.boundscheck(False)
+
+
 def fht(np.ndarray[DTYPE_t] array_):
+    """ Single dimensional fht. """
+    if not is_power_of_two(array_.shape[0]):
+        raise ValueError('Length of input for fht must be a power of two')
+    else:
+        _fht(array_)
+
+@cython.boundscheck(False)
+cdef _fht(np.ndarray[DTYPE_t, ndim=1] array_):
     cdef unsigned int bit, length, _, i, j
     cdef double temp
     bit = length = array_.shape[0]
-    if not is_power_of_two(length):
-        raise ValueError('Length of input for fht must be a power of two')
     for _ in xrange(<unsigned int>(log2(length))):
         bit >>= 1
         for i in xrange(length):
@@ -27,19 +35,20 @@ def fht(np.ndarray[DTYPE_t] array_):
                 array_[i] += array_[j]
                 array_[j] = temp - array_[j]
 
-@cython.boundscheck(False)
+
+
 def fht2(np.ndarray[DTYPE_t, ndim=2] array_):
+    if not is_power_of_two(array_.shape[1]):
+        raise ValueError('Length of rows for fht2 must be a power of two')
+    else:
+        _fht2(array_)
+
+@cython.boundscheck(False)
+cdef _fht2(np.ndarray[DTYPE_t, ndim=2] array_):
     cdef unsigned int bit, length, _, i, j, n
     cdef double temp
     n = array_.shape[0]
     for x in xrange(n):
-        bit = length = array_.shape[1]
-        for _ in xrange(<unsigned int>(log2(length))):
-            bit >>= 1
-            for i in xrange(length):
-                if i & bit == 0:
-                    j = i | bit
-                    temp = array_[x, i]
-                    array_[x, i] += array_[x, j]
-                    array_[x, j] = temp - array_[x, j]
-
+        # TODO: This call still shows up as yellow in cython -a presumably due
+        # to the [] access, but the array_ is already typed...
+        _fht(array_[x])
--- sklearn/utils/tests/test_fht.py
+++ sklearn/utils/tests/test_fht.py
@@ -3,6 +3,7 @@ import numpy.testing as npt
 import nose.tools as nt
 from scipy.linalg import hadamard
 from sklearn.utils.cyfht import fht as cyfht
+from sklearn.utils.cyfht import fht2 as cyfht2
 
 
 def test_wikipedia_example():
@@ -22,5 +23,16 @@ def test_numerical_fuzzing_fht():
         npt.assert_array_almost_equal(np.dot(copy, H), input_)
 
 
+def test_numerical_fuzzing_fht2():
+    for length in [2, 4, 8, 16, 32, 64]:
+        for rows in [1, 2, 3, 4, 5]:
+            input_ = np.random.normal(size=(rows, length))
+            copy = input_.copy()
+            H = hadamard(length)
+            cyfht2(input_)
+            npt.assert_array_almost_equal(np.dot(copy, H), input_)
+
+
 def test_exception_when_input_not_power_two():
     nt.assert_raises(ValueError, cyfht, np.zeros(9, dtype=np.float64))
+    nt.assert_raises(ValueError, cyfht2, np.zeros((2, 9), dtype=np.float64))
